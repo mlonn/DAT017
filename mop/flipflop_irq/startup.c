e78dd52f0298f820a668c0fc4e5c530eebb8b23f
Mikael Lönn <mikael.lonn@chorus.se>
2019-02-21 16:09:57 +0100

    flipflop

--- mop/flipflop_irq/startup.c
+++ mop/flipflop_irq/startup.c
@@ -2,6 +2,7 @@
  * 	startup.c
  *
  */
+ #include "gpio.h"
 void startup(void) __attribute__((naked)) __attribute__((section (".start_section")) );
 
 void startup ( void )
@@ -13,8 +14,52 @@ __asm volatile(
 	".L1: B .L1\n"				/* never return */
 	) ;
 }
+#define SYSCFG 0x40013800
+#define SYSCFG_EXTICR1 ((volatile unsigned int*)(SYSCFG + 0x8))
 
+#define EXTI 0x40013C00
+#define EXTI_IMR ((volatile unsigned int*)(EXTI))
+#define EXTI_RTSR ((volatile unsigned int*)(EXTI) + 0x8)
+#define EXTI_PR ((volatile unsigned int*)(EXTI) + 0x14)
+
+#define NVIC 0xE000E100
+#define NVIC_ISER0 ((volatile unsigned int*)(NVIC))
+#define NVIC_EXTI3_IRQ_BPOS (1<<9)
+int count;
+void irq_handler(void) {
+    count++;
+    if( (*EXTI_PR & NVIC_EXTI3_IRQ_BPOS) != 0 ) {
+        *EXTI_PR |= NVIC_EXTI3_IRQ_BPOS;
+    }
+}
+
+void app_init(void) {
+    /*SET OUTPUT FOR HEX DISP*/
+    GPIO_D.moder = 0x00005555;
+    /*SET INPUT FOR FLIPFLOP*/
+    GPIO_E.moder = 0x0;
+    
+    /*Nollställ fält*/
+    *SYSCFG_EXTICR1 &= 0x0FFF;
+    /*PE3 -> EXTI3*/
+    *SYSCFG_EXTICR1 |= 0x4000;
+    
+    *EXTI_IMR |= 4;
+    *EXTI_RTSR |= 4;
+    
+    *((void (**)(void) ) 0x2001C064 )  = irq_handler;
+    
+    *NVIC_ISER0 |= NVIC_EXTI3_IRQ_BPOS;
+    
+    count = 0;
+}
 void main(void)
 {
+    app_init();
+    while(1){
+        GPIO_D.odrLow = count;
+        GPIO_D.odrLow = count;
+        GPIO_D.odrLow = count;
+    }
 }
 
